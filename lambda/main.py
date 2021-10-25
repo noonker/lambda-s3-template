160e15be030f749e3214b439e86a5448b468a03f
Joshua Person <mailroom@legitimate.company>
2021-10-24 22:39:24 -0500

    Init

---
+++ lambda/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import boto3
+import logging
+import os
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+
+
+def handler(event, context):
+    # Read the file
+    s3 = boto3.client('s3')
+    inbucket = event['Records'][0]['s3']['bucket']['name']
+    fname = event['Records'][0]['s3']['object']['key']
+    log.info(event)
+    log.info(inbucket)
+    log.info(fname)
+    s3_res = s3.get_object(Bucket=inbucket, Key=fname)
+    infile  = s3_res['Body'].read()
+
+    # Output buckets
+    results_bucket = os.environ['RESULTS_BUCKET']
+    error_bucket = os.environ['ERROR_BUCKET']
+    source_bucket = os.environ['SOURCE_BUCKET']
+
+
+    if True: # Success
+        s3.put_object(Bucket=results_bucket, Key=f"{fname}.processed", Body=b"good")
+    else: # Error
+        s3.put_object(Bucket=error_bucket, Key=f"{fname}.processed", Body=b"bad")
+
+    # Cleanup
+    s3.delete_object(Bucket=inbucket, Key=fname)
+
+    return {
+        'message' : "success"
+    }
--- lib/lambda-s3-template-stack.ts
+++ lib/lambda-s3-template-stack.ts
@@ -1,9 +1,51 @@
+// TODO Email Notifications
+
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as lambdaEventSources from '@aws-cdk/aws-lambda-event-sources';
 
 export class LambdaS3TemplateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const sourceBucket = new s3.Bucket(this, 'sourceBucket', {
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const resultsBucket = new s3.Bucket(this, 'resultsBucket', {
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const errorBucket = new s3.Bucket(this, 'errorBucket', {
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'main.handler',
+      runtime: lambda.Runtime.PYTHON_3_9,
+    });
+
+    lambdaFunction.addEnvironment("RESULTS_BUCKET", resultsBucket.bucketName)
+    lambdaFunction.addEnvironment("ERROR_BUCKET", errorBucket.bucketName)
+    lambdaFunction.addEnvironment("SOURCE_BUCKET", sourceBucket.bucketName)
+
+
+    const s3PutEventSource = new lambdaEventSources.S3EventSource(sourceBucket, {
+      events: [
+        s3.EventType.OBJECT_CREATED_PUT
+      ]
+    });
+
+    lambdaFunction.addEventSource(s3PutEventSource);
+
+    sourceBucket.grantReadWrite(lambdaFunction)
+    errorBucket.grantReadWrite(lambdaFunction)
+    resultsBucket.grantReadWrite(lambdaFunction)
+    sourceBucket.grantRead(lambdaFunction)
   }
 }
